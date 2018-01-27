66098d03ac9d683241c0c719ebf9007efad62ab8
shaolin-wu <15265249516@163.com>
2018-01-27 23:56:59 +0800

    What:V2.4 使用MVC的写法改造代码
    Why:
    
    * 使项目的代码结构清晰
    
    How:
    
    * 使用view 和control，control里面都是属性和函数

--- js/smoothly-navigation.js
+++ js/smoothly-navigation.js
@@ -1,29 +1,25 @@
 !function () {
-    let aTags = document.querySelectorAll('nav > ul > li > a')
-    function animate(time) {
-        requestAnimationFrame(animate);
-        TWEEN.update(time);
-    }
-    requestAnimationFrame(animate);
-
-
-    for (let i = 0; i < aTags.length; i++) {
-        aTags[i].onclick = function (msg) {
-            msg.preventDefault()//阻止默认动作
-            // console.log(msg)
-            // console.log(msg.currentTarget)
-            let a = msg.currentTarget
-            //console.log(a.getAttribute('href'))//写的什么什么就是什么
-            // console.log(a.href)//带http协议
-            let href = a.getAttribute('href')
-            let element = document.querySelector(href)
-            let rect = element.getBoundingClientRect()
+    var view = document.querySelector('nav.menu')
+    var controller = {
+        view: null,
+        aTags: null,
+        init: function (view) {
+            this.view = view
+            this.initAnimation()
+            this.bindEvents()
+        },
+        initAnimation: function () {
+            function animate(time) {
+                requestAnimationFrame(animate);
+                TWEEN.update(time);
+            }
+            requestAnimationFrame(animate);
+        },
+        scrollElement: function (element) {
             let top = element.offsetTop
-
             let currentTop = window.scrollY //起始位置
             let targetTop = top - 80 //结束位置
             let s = targetTop - currentTop
-            //console.log(s)
             var coords = { y: currentTop };
             var t = Math.abs((s / 100) * 300);
             if (t > 500) { t = 500 }
@@ -34,26 +30,22 @@
                     window.scrollTo(0, coords.y)
                 })
                 .start();
+        },
+        bindEvents: function () {
+            let aTags = this.view.querySelectorAll('nav > ul > li > a')
 
-            // let n = 25 //一共动25次
-            // let t = 500 / n //多长时间动一次
-            // let currentTop = window.scrollY
-            // let targetTop = top - 80
-            // let S = targetTop - currentTop
-            // let s = S / n//每次移动的距离
-            // let i = 0
-            // // console.log(currentTop)
-            // // console.log(targetTop)
-            // // console.log(distance)
-            // let id = setInterval(() => {
-            //     if (i === n) {
-            //         window.clearInterval(id)
-            //         return
-            //     }
-            //     i = i + 1
-            //     window.scrollTo(0, currentTop + distance * i)
-            // }, t)
-
+            for (let i = 0; i < aTags.length; i++) {
+                aTags[i].onclick = (msg) => {
+                    msg.preventDefault()//阻止默认动作
+                    let a = msg.currentTarget
+                    let href = a.getAttribute('href')
+                    let element = document.querySelector(href)
+                    let rect = element.getBoundingClientRect()
+                    this.scrollElement(element)
+                }
+            }
         }
     }
+    
+    controller.init(view)
 }.call()
--- js/sticky-topbar.js
+++ js/sticky-topbar.js
@@ -1,7 +1,27 @@
-window.addEventListener('scroll', function (scroll) {
-    if (window.scrollY > 0) {
-        sticky.classList.add('sticky')
-    } else {
-        sticky.classList.remove('sticky')
+!function () {
+    var view = document.querySelector('.topNavBar')
+    var controller = {
+        view: null,
+        init: function (view) {
+            this.view = view
+            this.bindEvents()
+        },
+        bindEvents: function () {
+            var view = this.view
+            window.addEventListener('scroll', () => {
+                if (window.scrollY > 0) {
+                    this.active()
+                } else {
+                    this.deactive()
+                }
+            })
+        },
+        active: function () {
+            this.view.classList.add('sticky')
+        },
+        deactive: function () {
+            this.view.classList.remove('sticky')
+        }
     }
-})
+    controller.init(view)
+}.call()
