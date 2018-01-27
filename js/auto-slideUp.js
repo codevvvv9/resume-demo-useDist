fc8808708645150dd6c1096b501c14f455729355
shaolin-wu <15265249516@163.com>
2018-01-27 18:46:43 +0800

    What: V2.4 使用立即执行函数避免全局变量的污染,修复图片路径的bug
    Why:
    
    * 在ES 5中var存在变量提升,不同的js之间万一变量重名,后者会覆盖前者
    
    How:
    
    * 1、使用let 2、使用!fucntion () {}.call() 立即执行函数

--- js/auto-slideUp.js
+++ js/auto-slideUp.js
@@ -1,47 +1,49 @@
- //添加 offset类
- let specialTags = document.querySelectorAll('[data-x]')
- for (let i = 0; i < specialTags.length; i++) {
-    specialTags[i].classList.add('offset')
- }
+ !function () {
+     let specialTags = document.querySelectorAll('[data-x]')
+     for (let i = 0; i < specialTags.length; i++) {
+         specialTags[i].classList.add('offset')
+     }
 
- findCloestAddRemoveOffset() //没滑动就让他出现
- window.addEventListener('scroll', function (scroll) {
-     findCloestAddRemoveOffset()
- })
+     findCloestAddRemoveOffset() //没滑动就让他出现
+     window.addEventListener('scroll', function (scroll) {
+         findCloestAddRemoveOffset()
+     })
 
-/**
- * 找到最近的tag并移除offset类
- */
- function findCloestAddRemoveOffset(){
-    let specialTags = document.querySelectorAll('[data-x]')
-    let minIndex = 0
-    for (let i = 1; i < specialTags.length; i++) {
-        if (Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)) {
-            minIndex = i;
-        }
-    }
+     /**
+      * 找到最近的tag并移除offset类
+      */
+     function findCloestAddRemoveOffset() {
+         let specialTags = document.querySelectorAll('[data-x]')
+         let minIndex = 0
+         for (let i = 1; i < specialTags.length; i++) {
+             if (Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)) {
+                 minIndex = i;
+             }
+         }
 
-    specialTags[minIndex].classList.remove('offset')
-    let id = specialTags[minIndex].id
-    // console.log(id)
-    //根据id找对应的元素
-    let a = document.querySelector('a[href="#' + id + '"]')
-    let li = a.parentNode
-    let brothersAndMe = li.parentNode.children
-    for (let i = 0; i < brothersAndMe.length; i++) {
-        brothersAndMe[i].classList.remove('highlight')
-    }
-    li.classList.add('highlight')
-    // console.log(li)
-}
+         specialTags[minIndex].classList.remove('offset')
+         let id = specialTags[minIndex].id
+         // console.log(id)
+         //根据id找对应的元素
+         let a = document.querySelector('a[href="#' + id + '"]')
+         let li = a.parentNode
+         let brothersAndMe = li.parentNode.children
+         for (let i = 0; i < brothersAndMe.length; i++) {
+             brothersAndMe[i].classList.remove('highlight')
+         }
+         li.classList.add('highlight')
+         // console.log(li)
+     }
 
-let liTags = document.querySelectorAll('nav.menu > ul > li')
-for (let i = 0; i < liTags.length; i++) {
-    liTags[i].onmouseenter = function (msg) {
-        msg.currentTarget.classList.add('active')
-    }
-    liTags[i].onmouseleave = function (msg) {
-        msg.currentTarget.classList.remove('active')
-    }
+     let liTags = document.querySelectorAll('nav.menu > ul > li')
+     for (let i = 0; i < liTags.length; i++) {
+         liTags[i].onmouseenter = function (msg) {
+             msg.currentTarget.classList.add('active')
+         }
+         liTags[i].onmouseleave = function (msg) {
+             msg.currentTarget.classList.remove('active')
+         }
 
-}
+     }
+}.call()//添加 offset类
+ 
