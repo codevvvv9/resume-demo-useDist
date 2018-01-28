3ec0e2aca843f4d33ed07fd4309dc0ca4563f669
shaolin-wu <15265249516@163.com>
2018-01-28 13:30:29 +0800

    What: V3.1 页面显示提交留言并显示留言板，略粗糙
    Why:
    
    * 直观啊
    
    How:
    
    * form表单提交， 使用LeanCloud的var query = new AV.Query('Message'); query.find().then()

--- js/message.js
+++ js/message.js
@@ -5,7 +5,43 @@ AV.init({
   appId: APP_ID,
   appKey: APP_KEY
 });
-console.log('运行到这里没有错')
+
+var query = new AV.Query('Message');
+query.find()
+    .then(
+        function (messages) {
+            let array = messages.map(function (item) {
+                return item.attributes
+            })
+            array.forEach((item) => {
+                let li = document.createElement('li')
+                li.innerText = item.content
+                let messagesList = document.querySelector('#messagesList')
+                messagesList.appendChild(li)
+            })
+        },
+        function (error) {
+            alert('提交失败， 请改天来留言')
+        })
+    .then(() => {}, (error) => {
+        console.log(error)
+    });
+let myForm = document.querySelector('#postMessageForm')
+
+myForm.addEventListener('submit', function (e) {
+    e.preventDefault()
+    let content = myForm.querySelector('input[name=content]').value
+    
+    var Message = AV.Object.extend('Message');
+    var message = new Message();
+    message.save({
+        'content': content
+    }).then(function (object) {
+        window.location.reload()
+        console.log(object);
+    })
+
+})
 
 // //创建 TestObject表
 // var TestObject = AV.Object.extend('Wushao');
